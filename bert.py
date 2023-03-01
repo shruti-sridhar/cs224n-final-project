1ebe4b42f64bfcdd6eb5f66e94c29235c3666c7e
Lisa Yi <51042110+lisayi101@users.noreply.github.com>
2023-03-01 11:14:32 -0800

    updates

--- bert.py
+++ bert.py
@@ -124,8 +124,16 @@ class BertLayer(nn.Module):
     3. a feed forward layer
     4. a add-norm that takes the input and output of the feed forward layer
     """
-    ### TODO
-    raise NotImplementedError
+    output1 = self.self_attention(hidden_states,attention_mask)
+    # add-norm layer
+    output2 = self.add_norm(hidden_states, output1, self.attention_dense, self.attention_dropout, self.attention_layer_norm)
+    # feed forward
+    output3 = self.interm_dense(output2)
+    output3 = self.interm_af(output3)
+    # another add-norm layer
+    ff_layer = self.add_norm(output2, output3, self.out_dense, self.out_dropout, self.out_layer_norm)
+
+    return ff_layer
 
 
 
